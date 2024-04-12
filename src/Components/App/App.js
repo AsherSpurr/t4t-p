132e5ba528f6768fc94f1b6799a9495c65b25aa3
Asher Spurr <144856487+AsherSpurr@users.noreply.github.com>
2024-04-12 16:15:00 -0600

    idk

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,16 +1,17 @@
-import { Routes, Route } from 'react-router-dom'
+// import { Routes, Route } from 'react-router-dom'
 import './App.css';
 
 function App() {
+  console.log(process.env.REACT_APP_GOOGLE)
+  console.log('wtf')
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <nav>
+          <h1>{}</h1>
         </nav>
-      </header>
-      <Routes>
-        <Route to='./'/>
-      </Routes>
+      </header> */}
+      <h1>hello</h1>
     </div>
   );
 }
