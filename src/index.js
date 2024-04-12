43c1fa993fc3bdad8b92fd3d2aa3bf78efbaf3a0
Asher Spurr <144856487+AsherSpurr@users.noreply.github.com>
2024-04-12 16:15:00 -0600

    idk

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './Components/App/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+const dotenv = require("dotenv")
+dotenv.config()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
