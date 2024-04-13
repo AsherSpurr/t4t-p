ba32a6c180ba29357c0780bef85ee8dbf3b07791
Asher Spurr <144856487+AsherSpurr@users.noreply.github.com>
2024-04-12 19:29:21 -0600

    Chore: move fetchBRbyLoc to appropriate component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,24 +4,11 @@ import About from '../about/About'
 import Landing from '../landing/Landing'
 import Error from '../error/Error'
 import LocDetails from '../locDetails/LocDetails'
-import { useEffect } from 'react';
-import { fetchBRsByLocNoParams } from '../../apiCalls';
+// import { useEffect } from 'react';
 
 
 function App() {
-  const lat = 39.753604
-  const lon = -104.428580
-  useEffect(() => {
-      fetch()
-    }, [])
-   async function fetch() {
-    try {
-      const tryFetch = await fetchBRsByLocNoParams(lat, lon)
-      console.log(tryFetch)
-    } catch(error) {
-      throw error
-    }
-   }
+
   return (
     <div className="App">
       <header>
