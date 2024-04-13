3d92f4d3206a8189391965146cc18f51311c2a97
Asher Spurr <144856487+AsherSpurr@users.noreply.github.com>
2024-04-12 18:49:42 -0600

    Feat: add empty routes

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,27 @@
-// import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, NavLink } from 'react-router-dom'
 import './App.css';
+import About from '../about/About'
+import Landing from '../landing/Landing'
+import Error from '../error/Error'
+import LocDetails from '../locDetails/LocDetails'
 
 function App() {
-  // const idk = process.env.REACT_APP_GOOGLE
   return (
     <div className="App">
-    
+      <header>
+        <nav>
+          <h1>T4Tp</h1>
+          <NavLink></NavLink>
+        </nav>
+      </header>
+      <main className='main'>
+        <Routes>
+          <Route to='/' element={<Landing />}/>
+          <Route to='/About' element={<About />}/>
+          <Route to='/Error' element={<Error />}/>
+          <Route to='/:locationName' element={<LocDetails />}/>        
+        </Routes>
+      </main>
     </div>
   );
 }
