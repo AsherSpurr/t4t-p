662dd6230e13c14912de43ca1490d7395bc5b5b3
Asher Spurr <144856487+AsherSpurr@users.noreply.github.com>
2024-04-15 11:17:12 -0600

    Add bathrooms fixture

--- cypress/e2e/landing.cy.js
+++ cypress/e2e/landing.cy.js
@@ -1,16 +1,19 @@
 describe('landing page', () => {
   beforeEach(() => {
-    //Bennett
-// 39.6702347
-//-104.4714657
+ const key = process.env.REACT_APP_GOOGLE
     cy.intercept('GET', 'https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=0&per_page=10&offset=0&lat=39.6702347&lng=-104.4714657', {
-      statusCode: 201,
-      body: {
-        
-      }
-    })
+      statusCode: 200,
+      fixture: 'bathrooms'
+    }).as('getBathrooms')
+    cy.intercept('GET', `https://maps.googleapis.com/maps/api/geocode/json?address=44301%20EIliff%20trail%20Bennett%20CO&key=${key}`, {
+      statusCode: 200,
+      fixture: 'bennett'
+    }).as('getBennett')
+    cy.visit('https://localhost:3000/')
+    // cy.wait('@getBennett')
+    // cy.wait('@getBathrooms')
   })
   it('should show landing page ui', () => {
-    cy.visit('https://example.cypress.io')
+    // cy.visit('https://localhost:3000/')
   })
 })
