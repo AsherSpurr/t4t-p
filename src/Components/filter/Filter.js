8e1eb9b422084c44f4a28bf5e4ce6ec7c6930962
Asher Spurr <144856487+AsherSpurr@users.noreply.github.com>
2024-04-15 10:52:18 -0600

    Chore: adjust UI temporarily to fit new form

--- src/Components/filter/Filter.js
+++ src/Components/filter/Filter.js
@@ -1,11 +1,14 @@
 import './Filter.css';
+// import truesvg from '../images/true.svg'
+// import falsesvg from '../images/false.svg'
+// import distance from '../images/distance-blue.svg'
 
 const Filter = () => {
     return (
         <div className='Filter_div_wrapper'>
-            {/* <button className='Filter_button'>A</button>
-            <button className='Filter_button'>B</button>
-            <button className='Filter_button'>C</button> */}
+            <button className='Filter_button'>Accessible</button>
+            <button className='Filter_button'>Unisex</button>
+            <button className='Filter_button'>Changing Table</button>
         </div>
     )
 }
