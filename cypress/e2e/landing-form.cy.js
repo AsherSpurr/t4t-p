8c2cf7b7490d8a59e677ed2a598874c4bc100481
Asher Spurr <144856487+AsherSpurr@users.noreply.github.com>
2024-04-17 10:30:41 -0600

    Huh

---
+++ cypress/e2e/landing-form.cy.js
@@ -0,0 +1,45 @@
+describe('landing page form', () => {
+  beforeEach(() => {
+ const key = process.env.REACT_APP_GOOGLE
+ //Delete this empty fetch after fixing known issue
+    cy.intercept('GET', 'https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=0&per_page=30&offset=0&lat=&lng=', {
+    statusCode: 200,
+    }).as('getEmptyBathrooms')
+    cy.intercept('GET', 'https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=0&per_page=30&offset=0&lat=39.6702347&lng=-104.4714657', {
+      statusCode: 200,
+      fixture: 'bathrooms'
+    }).as('getBathrooms')
+    cy.intercept('GET', `https://maps.googleapis.com/maps/api/geocode/json?address=44301EIlifftrail%20Bennett%20CO&key=${key}`, {
+      statusCode: 200,
+      fixture: 'bennett'
+    }).as('getBennett')
+    cy.visit('http://localhost:3000/')
+    // cy.wait('@getBennett')
+    // cy.wait('@getBathrooms')
+  })
+
+  it('should type into the form and show list of locations', () => {
+    //Test type into form
+    cy.get('form')
+    .get(`input[name='street']`).type('44301EIlifftrail').should('have.value', '44301EIlifftrail')
+    .get(`input[name='town']`).type('Bennett').should('have.value', 'Bennett')
+    .get(`input[name='state']`).type('CO').should('have.value', 'CO')
+    .get('.Search_button').click()
+
+    //Test that locations display after click
+    cy.wait('@getBathrooms')
+    .get('.Locations_div_wrapper').children().should('have.length', '30')
+    .get('.Card_div_container').first().contains('Subway')
+    .get('.Card_div_container').last().contains('insert name of location')
+  })
+  //Move into own describe block
+  it('should navigate to the location details page and show contents when card is clicked', () => {
+    //Test click from landing page
+    //Test URL
+    //Test contents
+    //Test any interactive elements
+    //Test click back to home
+    //Confirm home URL
+  })
+
+})
--- cypress/e2e/landing.cy.js
+++ cypress/e2e/landing.cy.js
@@ -2,14 +2,14 @@ describe('landing page', () => {
   beforeEach(() => {
  const key = process.env.REACT_APP_GOOGLE
  //Delete this empty fetch after fixing known issue
-    cy.intercept('GET', 'https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=0&per_page=10&offset=0&lat=&lng=', {
+    cy.intercept('GET', 'https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=0&per_page=30&offset=0&lat=&lng=', {
     statusCode: 200,
     }).as('getEmptyBathrooms')
-    cy.intercept('GET', 'https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=0&per_page=10&offset=0&lat=39.6702347&lng=-104.4714657', {
+    cy.intercept('GET', 'https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=0&per_page=30&offset=0&lat=39.6702347&lng=-104.4714657', {
       statusCode: 200,
       fixture: 'bathrooms'
     }).as('getBathrooms')
-    cy.intercept('GET', `https://maps.googleapis.com/maps/api/geocode/json?address=44301%20EIliff%20trail%20Bennett%20CO&key=${key}`, {
+    cy.intercept('GET', `https://maps.googleapis.com/maps/api/geocode/json?address=44301EIlifftrail%20Bennett%20CO&key=${key}`, {
       statusCode: 200,
       fixture: 'bennett'
     }).as('getBennett')
@@ -21,23 +21,36 @@ describe('landing page', () => {
   it('should show landing page ui', () => {
     cy.get('nav').contains('T4Tp')
     .get('nav').get('a').contains('About') 
+    .get('nav').get('img').should('have.attr', 'src').should('include', '/static/media/distance-blue')
     .get('main').get('h2').contains('Where do you want to')
     //Test length of inputs
-    .get('form').children().should('have.length', '6')
-    .get('form').get(`input[name='num']`)
-    .get('form').get(`input[name='street']`)
-    .get('form').get(`input[name='streetIdent']`)
-    .get('form').get(`input[name='town']`)
-    .get('form').get(`input[name='state']`)
-    .get('main').get('img').should('have.attr', 'src').should('include', '/static/media/placeholder2')
+    // .get('form').children().should('have.length', '4')
+    // .get('form').get('.Search_button').get('.Search_icon').should('have.attr', 'src').should('include', '/static/media/search')
+    // .get('form').get(`input[name='street']`)
+    // .get('form').get(`input[name='town']`)
+    // .get('form').get(`input[name='state']`)
+    // .get('form').get('.Search_button')
     //Test click + navigate to About + URL
+    .get('.Filter_div_wrapper').children().should('have.length', '3')
+    .get('.Filter_button').first().should('have.id', 'accessible')
+    // .get('.Filter_button').last().should('have.id', 'all')
+    .get('.Map_div_wrapper').get('.Map_img').should('have.attr', 'src').should('include', '/static/media/placeholder-gray2')
   })
 
-  it('should type into the form and show list of locations', () => {
-    //Test type into form
-    //Test button click
-    //Test first and last locations + contents
-  })
+  // it('should type into the form and show list of locations', () => {
+  //   //Test type into form
+  //   cy.get('form')
+  //   .get(`input[name='street']`).type('44301EIlifftrail').should('have.value', '44301EIlifftrail')
+  //   .get(`input[name='town']`).type('Bennett').should('have.value', 'Bennett')
+  //   .get(`input[name='state']`).type('CO').should('have.value', 'CO')
+  //   .get('.Search_button').click()
+
+  //   //Test that locations display after click
+  //   cy.wait('@getBathrooms')
+  //   .get('.Locations_div_wrapper').children().should('have.length', '30')
+  //   .get('.Card_div_container').first().contains('Subway')
+  //   .get('.Card_div_container').last().contains('insert name of location')
+  // })
   //Move into own describe block
   it('should navigate to the About page and show contents', () => {
     //Test click from landing page
