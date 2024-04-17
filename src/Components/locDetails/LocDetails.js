d24745550cc5ec4b6c61778f326936fa9587dfdd
Asher Spurr <144856487+AsherSpurr@users.noreply.github.com>
2024-04-16 22:11:37 -0600

    Feat: add map placeholder to details

--- src/Components/locDetails/LocDetails.js
+++ src/Components/locDetails/LocDetails.js
@@ -1,24 +1,33 @@
-import './LocDetails.css';
-import Card from '../card/Card';
-import { useParams } from 'react-router-dom'
+import "./LocDetails.css";
+import Card from "../card/Card";
+import Map from "../map/Map";
+import { useParams } from "react-router-dom";
 
 const LocDetails = ({ filteredLocs }) => {
-    const locName = useParams().locationName
-    const loc = filteredLocs.find((loc) => {
-        return loc.name === locName
-    })
+  const locName = useParams().locationName;
+  const loc = filteredLocs.find((loc) => {
+    return loc.name === locName;
+  });
 
-    return (
-        <>
-            <Card 
-              name={loc.name}   
-              uni={loc.unisex}
-              access={loc.accessible}
-              dist={loc.distance}  
-              key={loc.id}  
-            />
-        </>
-    )
-}
+  return (
+    <div className="Landing_wrapper">
+      <div className="Landing_left_wrapper">
+        <h2>{loc.name}</h2>
+        <div className="Locations_div_wrapper">
+          <Card
+            name={loc.name}
+            uni={loc.unisex}
+            access={loc.accessible}
+            dist={loc.distance}
+            key={loc.id}
+          />
+        </div>
+      </div>
+      <div className="Landing_map_wrapper">
+        <Map />
+      </div>
+    </div>
+  );
+};
 
-export default LocDetails
+export default LocDetails;
