006ca02ed69dce6ef92ea068fd7131ffb3528578
Asher Spurr <144856487+AsherSpurr@users.noreply.github.com>
2024-04-17 18:53:43 -0600

    Filter state updates accordingly

--- src/Components/filter/Filter.js
+++ src/Components/filter/Filter.js
@@ -65,15 +65,15 @@ const Filter = ({ updateLocs, locs, setFilters, updateFilters }) => {
     return (
         <form className='Filter_div_wrapper' >
             {/* <div> */}
-                <input type='checkbox' className='Filter_input' id='accessible' name='accessible' value={activeFilters.accessible} onChange={(e) => handleChange(e)}/>
+                <input type='checkbox' className='Filter_input' id='accessible' name='accessible' value={activeFilters.accessible} onChange={handleChange}/>
                 <label for='accessible'>Accessible</label>
             {/* </div>
             <div> */}
-                <input type='checkbox' className='Filter_input' id='unisex'name='unisex' value={activeFilters.unisex} onChange={(e) => handleChange(e)}/>
+                <input type='checkbox' className='Filter_input' id='unisex'name='unisex' value={activeFilters.unisex} onChange={handleChange}/>
                 <label for='unisex'>Unisex</label>
             {/* </div>
             <div> */}
-                <input type='checkbox' className='Filter_input' id='all' name='all' value={activeFilters.all} onChange={(e) => handleChange(e)}/>
+                <input type='checkbox' className='Filter_input' id='all' name='all' value={activeFilters.all} onChange={handleChange}/>
                 <label for='all'>All</label>
             {/* </div>        */}
         </form>
