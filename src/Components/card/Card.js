42b4d9bdfe17c0bd20356121b79724c5c418b310
Asher Spurr <144856487+AsherSpurr@users.noreply.github.com>
2024-04-19 14:36:39 -0600

    Update Card tags and alt tags

--- src/Components/card/Card.js
+++ src/Components/card/Card.js
@@ -18,7 +18,7 @@ const Card = ({ name, uni, access, dist, city, state, upvote, downvote, directio
 
     return (
             <div className={locName === name ? 'LocDetails_div_wrapper' : 'Card_div_wrapper'}>
-            <div className={locName === name ? 'LocDetails_div_container' : `Card_div_container`} onClick={() => navigate(`/locations/${name}`)}>
+            <section className={locName === name ? 'LocDetails_div_container' : `Card_div_container`} onClick={() => navigate(`/locations/${name}`)}>
                 <div className={locName === name ? 'LocDetails_img_container' : 'Card_img_container'}>
                     <span className={locName === name ? 'LocDetails_img_placeholder' : 'Card_img_placeholder'}></span>
                 </div>
@@ -27,33 +27,38 @@ const Card = ({ name, uni, access, dist, city, state, upvote, downvote, directio
                     <><div className='LocDetails_heading_container'>
                         <h3 className='LocDetails_h3'>{city} | {state}</h3> 
                         <div className='LocDetails_icons_container'>
-                            <img id='LocDetails_icon_upvote' src={upvotesvg} alt='' height='20px' width='20px'></img>
+                            <img id='LocDetails_icon_upvote' src={upvotesvg} alt='Thumbs up upvote' height='20px' width='20px'></img>
                             <p className='LocDetails_p'>{upvote}</p>
-                            <img id='LocDetails_icon_downvote' src={downvotesvg} alt='' height='20px' width='20px'></img>
+                            <img id='LocDetails_icon_downvote' src={downvotesvg} alt='Thumbs down downvote' height='20px' width='20px'></img>
                             <p className='LocDetails_p'>{downvote}</p>
                         </div>
                         </div></>
                     : <h3 className='Card_h3'>{name}</h3>}
                     <div className={locName === name ? 'LocDetails_icons_container' : 'Card_icons_container'}>
                         <div className={locName === name ? 'LocDetails_distance_container' : 'Card_distance_container'}>
-                            <img src={distance} alt='' height='20px' width='20px'></img>
-                            <p className={locName === name ? 'LocDetails_p' : 'Card_p'}>{distRounded}m</p>
+                            <p className={locName === name ? 'LocDetails_p' : 'Card_p'}>
+                                <img src={distance} alt='distance' height='20px' width='20px'/>{distRounded}m
+                           </p>
                         </div>
                         <div className={locName === name ? 'LocDetails_params_icon_container' : 'Card_params_icon_container'}>
-                            {uni ? <img id={locName === name ? 'LocDetails_icon_unisex' : 'Card_icon_unisex'} src={truesvg} alt='' height='20px' width='20px'></img> : <img id='Card_icon_unisex' src={falsesvg} alt='' height='20px' width='20px'></img>}
-                            <p className={locName === name ? 'LocDetails_p' : 'Card_p'}>Unisex</p>
-                            {access ? <img id={locName === name ? 'LocDetails_icon_accessible' : 'Card_icon_unisex'} src={truesvg} alt='' height='20px' width='20px'></img> : <img id='Card_icon_unisex' src={falsesvg} alt='' height='20px' width='20px'></img>}
-                            <p className={locName === name ? 'LocDetails_p' : 'Card_p'}>Accessible</p>
+                        <p className={locName === name ? 'LocDetails_p' : 'Card_p'}>
+                            {uni ? <img id={locName === name ? 'LocDetails_icon_unisex' : 'Card_icon_unisex'} src={truesvg} alt='Status of true' height='20px' width='20px'/> : <img id='Card_icon_unisex' src={falsesvg} alt='Status of false' height='20px' width='20px'/>}
+                            Unisex
+                        </p>
+                        <p className={locName === name ? 'LocDetails_p' : 'Card_p'}>
+                            {access ? <img id={locName === name ? 'LocDetails_icon_accessible' : 'Card_icon_unisex'} src={truesvg} alt='Status of true' height='20px' width='20px'/> : <img id='Card_icon_unisex' src={falsesvg} alt='Status of false' height='20px' width='20px'/>}
+                            Accessible
+                        </p>
                         </div>
                     </div>
                     {locName === name &&
-                        <div className='LocDetails_moreInfo_wrapper'>
+                        <article className='LocDetails_moreInfo_wrapper'>
                             <p>Directions: {directions}</p>
                             <p>Comment: {comment}</p>
-                        </div>
+                        </article>
                     }
                 </div>
-            </div>
+            </section>
         </div>
     )
 }
