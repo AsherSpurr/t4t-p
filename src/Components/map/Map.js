73b3ae7a559e995043d2f38c3c09b752190bc483
Asher Spurr <144856487+AsherSpurr@users.noreply.github.com>
2024-04-19 13:48:06 -0600

    Add multiple markers

--- src/Components/map/Map.js
+++ src/Components/map/Map.js
@@ -1,25 +1,34 @@
-import './Map.css';
-// import placeholder from '../images/placeholder-gray2.svg'
-// import { Loader } from "@googlemaps/js-api-loader"
-import {APIProvider, Map, Marker, useMapsLibrary} from '@vis.gl/react-google-maps';
+import "./Map.css";
+import { useLocation } from "react-router-dom";
+import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
 
+const GoogleMap = ({
+  position,
+  singlePosition,
+  singleZoom,
+  allLocsCoordinates,
+}) => {
+  const path = useLocation().pathname;
 
-const GoogleMap = ({ position, singlePosition, singleZoom, allLocsCoordinates } ) => {
-    
-    function markerCluster() {
-        const markerClusterLibrary = useMapsLibrary
-    }
-    // {allLocsCoordinates.map((loc) => {
+  return (
+    <APIProvider apiKey={process.env.REACT_APP_GOOGLE}>
+      <Map
+        defaultCenter={position}
+        center={singlePosition}
+        defaultZoom={10}
+        zoom={singleZoom}
+        className="Map_div_wrapper"
+      >
+        {!path.includes("locations") ? (
+          allLocsCoordinates.map((loc) => {
+            return <Marker position={loc} />;
+          })
+        ) : (
+          <Marker defaultPosition={position} position={singlePosition} />
+        )}
+      </Map>
+    </APIProvider>
+  );
+};
 
-    // })}
-    console.log(allLocsCoordinates)
-    return (
-        <APIProvider apiKey={process.env.REACT_APP_GOOGLE}>
-        <Map defaultCenter={position} center={singlePosition} defaultZoom={10} zoom={singleZoom} className='Map_div_wrapper'>
-          <Marker defaultPosition={position} position={singlePosition}/>
-        </Map>
-      </APIProvider>
-    )
-}
-
-export default GoogleMap
+export default GoogleMap;
