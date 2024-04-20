889a80fbfbc2eb30d901104677430df5617bdc71
Asher Spurr <144856487+AsherSpurr@users.noreply.github.com>
2024-04-19 18:27:04 -0600

    Quick spelling fix

--- src/Components/about/About.js
+++ src/Components/about/About.js
@@ -5,7 +5,7 @@ const About = () => {
         <article className='About_container'>
             <h2>What does T4Tp do?</h2>
             <strong><p>T4Tp is sort of a 'IYKYK application'</p></strong>
-            <p>We aim to provide compatable and safe(er) bathroom options to transgener individuals!</p>
+            <p>We aim to provide compatable and safe(er) bathroom options to transgender individuals!</p>
         </article>
     )
 }
